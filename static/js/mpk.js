9640201f568c222ed5494a0910dc3e1d6342e4a0
michaelpkuhn <rakuhn14@gmail.com>
2020-10-31 12:26:08 -0500

    dropdown added

---
+++ static/js/mpk.js
@@ -0,0 +1,11 @@
+
+function collapse(id){
+    elem = document.getElementById(id);
+    display = elem.style.display
+    if (display) {
+        elem.style.display = ""
+    }
+    else{
+        elem.style.display = "block"
+    }
+}
