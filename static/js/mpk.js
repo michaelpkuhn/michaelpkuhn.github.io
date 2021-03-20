93b1c21a80c2e1dca060e792890875e697d7a119
michaelpkuhn <mickuhn95@gmail.com>
2021-03-20 15:02:49 -0500

    cleaned up code, tweaks

--- static/js/mpk.js
+++ static/js/mpk.js
@@ -1,3 +1,4 @@
+// Page Resource Data Structure
 const resources = {
     "resume" : {
         "header_html" : "Resume",
@@ -9,8 +10,8 @@ const resources = {
     }
 }
 
+// Adds Event Listener to elements with nav_button class
 const page_buttons = document.querySelectorAll(".nav_button")
-console.log(page_buttons)
 page_buttons.forEach(element => 
     element.parentElement.addEventListener("click", loadPage))
 
@@ -18,21 +19,25 @@ function loadPage(click){
     // Sets span element (click element depends on where user clicks)
     let click_elem = click.srcElement
     let span = click_elem.localName === "li" ? click_elem.firstElementChild :
-                                               click_elem 
+                                               click_elem;
+
+    // Does nothing if the clicked page is the same as the active page
+    let active = document.querySelector(".active")
+    if (active === parent) {return null}
 
     // Grab Elements
     let header = document.querySelector("#pdf_viewer h2")
     let iframe = document.querySelector("#pdf_viewer iframe")
-    let active = document.querySelector(".active")
-    
-    const page = resources[span.id]
     let parent = span.parentElement
 
-    // console.log(resources[button.id])
-    if (active === parent) {return null}
-    // console.log(button.parentNode)
+    // Uses span id to get resource data object
+    const page = resources[span.id]
     
-    // Update elements
+    /*
+    Sets current active button to inactive
+    Sets clicked button to active
+    Updates the page header and iframe path reference
+    */
     active.setAttribute("class", "inactive")
     parent.setAttribute("class", "active")
     header.innerHTML = page.header_html
